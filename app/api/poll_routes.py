e6c988c536209ba81b74192e7e26843e4c0a7938
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-24 19:53:13 -0700

    check on get route

--- app/api/poll_routes.py
+++ app/api/poll_routes.py
@@ -7,16 +7,6 @@ from ..forms.poll_form import CreatePollForm
 
 poll_routes = Blueprint('polls', __name__)
 
-def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
-    errorMessages = []
-    for field in validation_errors:
-        for error in validation_errors[field]:
-            errorMessages.append(f'{field} : {error}')
-    return errorMessages
-
 # Get all polls
 
 @poll_routes.route('/')
