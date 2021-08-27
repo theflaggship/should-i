d3cfd9ee41c499aac16bc172919ed781b4088d54
Monte <36746765+theflaggship@users.noreply.github.com>
2021-08-27 16:01:04 -0700

    profile side bar working

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import LogoutButton from '../auth/LogoutButton';
@@ -6,26 +6,42 @@ import './NavBar.css'
 
 const NavBar = () => {
   const sessionUser = useSelector(state => state?.session.user)
+  const [options, setOptions] = useState(false);
+
   const id = sessionUser?.id
   return (
-    <div className="navbar-container">
-      <div className="left-nav">
-        <NavLink to='/' exact={true} activeClassName='active'>
-            <img className="nav-logo" src="https://i.imgur.com/zJ1Mb0C.png"/>
-        </NavLink>
-      </div>
-      <div className="center-nav">
-        <div className="search-bar-container">
-          <form>
-            <input placeholder="Search for users or categories" className="search-input"></input>
-          </form>
+    <div>
+      <div className="navbar-container">
+        <div className="left-nav">
+          <NavLink to='/' exact={true} activeClassName='active'>
+              <img className="nav-logo" src="https://i.imgur.com/zJ1Mb0C.png"/>
+          </NavLink>
+        </div>
+        <div className="center-nav">
+          <div className="search-bar-container">
+            <form>
+              <input placeholder="Search for users or categories" className="search-input"></input>
+            </form>
+          </div>
+        </div>
+        <div className="right-nav">
+          <div className="nav-user-profile-container">
+            <div onClick={()=> setOptions(!options)} className="nav-profile-pic-container">
+              <img className="nav-profile-pic" src={sessionUser.profile_pic}/>
+            </div>
+          </div>
+          {/* <NavLink to={`/users/${sessionUser?.id}`} className="profile-page-button">My Profile</NavLink>
+          <LogoutButton /> */}
         </div>
       </div>
-      <div className="right-nav">
-        <NavLink to={`/users/${sessionUser?.id}`} className="profile-page-button">My Profile</NavLink>
-        <LogoutButton />
+      <div className="sidebar" style={!options ? {transform: 'translateX(-110%)'} : {}}>
+        <div className='sidebar-container'>
+          <p className="nav-username">{sessionUser.username}</p>
+          <div className="arrow-button" onClick={()=> setOptions(!options)}>
+          <i class="fas fa-chevron-right"></i>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
